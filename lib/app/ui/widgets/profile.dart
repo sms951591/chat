4c7672acf2f9dbb47152252c03c205cf3599be41
Sung Minseok <sms8267@codeblue.co.kr>
2025-03-18 23:25:22 +0900

    asdasd
    asdasd

--- lib/app/ui/widgets/profile.dart
+++ lib/app/ui/widgets/profile.dart
@@ -37,7 +37,7 @@ class Profile extends StatelessWidget {
               ),
             ],
 
-          Expanded(asdasdasdasdasdasdasdasd
+          Expanded(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: isMe ? CrossAxisAlignment.start : CrossAxisAlignment.end,
